000da50845128227c2f77d41ae1ade1055abb6bd
Milad Sayad <miladsayad@gmail.com>
2019-10-23 17:33:24 +1100

    Finish a day

--- dist/moodle.js
+++ dist/moodle.js
@@ -2,6 +2,8 @@ var FITMOODLE = (function() {
 	// ***************************************************** Defaults variables ***************************************************** //
 
 	var MoodleBaseUrl = 'https://lms.monash.edu',
+		unitguideBaseUrl = 'https://unitguidemanager.monash.edu/',
+		unitguideSearchQuery = 'faculty=FACULTY+OF+INFORMATION+TECHNOLOGY',
 		tpDictonary = {
 			S1: 'S1-01',
 			S2: 'S2-01',
@@ -21,83 +23,81 @@ var FITMOODLE = (function() {
 			'MO-TP6': 'MO-TP6-01'
 		},
 		powerUsers = [],
-		avoidRestrictionQuery = 'restriction=off',
-		unitguideBaseUrl = 'https://unitguidemanager.monash.edu/',
-		unitguideSearchQuery = 'faculty=FACULTY+OF+INFORMATION+TECHNOLOGY';
-	
-		// ***************************************************** Utilities ***************************************************** //
+		bypassRestrictionQuery;
 
-		var getQueryVariable = function(variable) {
-			var query = window.location.search.substring(1);
-			var vars = query.split('&');
-			for (var i = 0; i < vars.length; i++) {
-				var pair = vars[i].split('=');
-				if (pair[0] == variable) {
-					return pair[1];
-				}
+	// ***************************************************** Utilities ***************************************************** //
+
+	var getQueryVariable = function(variable) {
+		var query = window.location.search.substring(1);
+		var vars = query.split('&');
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split('=');
+			if (pair[0] == variable) {
+				return pair[1];
 			}
-			return false;
-		};
-	
-		var moodlePortalButton = function(id) {
-			if (id) {
-				return {
-					elementHref: MoodleBaseUrl + "/course/view.php?id=" + id,
-					elementText: 'IT Student Portal'
-				};
-			} else {
-				return {
-					elementHref: MoodleBaseUrl,
-					elementText: 'Moodle Dashboard'
-				};
-			}
-		};
-	
-		// returns Unit Guide Button info given Unit Code, Teaching Period, and Year
-		var unitGuideButton = function(unitCode, tpCode, tpYear) {
-			if (unitCode && tpCode && tpYear) {
-				return {
-					elementHref:
-						unitguideBaseUrl +
-						'view?unitCode=' +
-						unitCode +
-						'&tpCode=' +
-						tpDictonary[tpCode] +
-						'&tpYear=' +
-						tpYear,
-					elementText: unitCode + ' Unit Guide'
-				};
-			} else {
-				return {
-					elementHref: unitguideBaseUrl + 'refine?' + unitguideSearchQuery,
-					elementText: 'Search Unit Guides'
-				};
-			}
-		};
-	
-		// Add a button
-		function addButtonToElement({
-			parentElementId = 'ExternalLink',
-			elementClass = 'btn btn-link btn-sm btn-block quick-link-button',
-			elementHref,
-			elementText
-		}) {
-			document.getElementById(parentElementId).innerHTML +=
-				'<a class="' +
-				elementClass +
-				'" href="' +
-				elementHref +
-				'" role="button" target="_blank">' +
-				elementText +
-				'</a>';
 		}
-	
-		function consoleHealthCheck(object, text) {
-			!Object.values(object).every((o) => o === null)
-				? console.log('%c PASSED ', 'color: white; background-color: #95B46A', text, object)
-				: console.log('%c ERROR ', 'color: white; background-color: #D33F49', text, object);
+		return false;
+	};
+
+	var moodlePortalButton = function(id) {
+		if (id) {
+			return {
+				elementHref: MoodleBaseUrl + '/course/view.php?id=' + id,
+				elementText: 'IT Student Portal'
+			};
+		} else {
+			return {
+				elementHref: MoodleBaseUrl,
+				elementText: 'Moodle Dashboard'
+			};
 		}
-	
+	};
+
+	// returns Unit Guide Button info given Unit Code, Teaching Period, and Year
+	var unitGuideButton = function(unitCode, tpCode, tpYear) {
+		if (unitCode && tpCode && tpYear) {
+			return {
+				elementHref:
+					unitguideBaseUrl +
+					'view?unitCode=' +
+					unitCode +
+					'&tpCode=' +
+					tpDictonary[tpCode] +
+					'&tpYear=' +
+					tpYear,
+				elementText: unitCode + ' Unit Guide'
+			};
+		} else {
+			return {
+				elementHref: unitguideBaseUrl + 'refine?' + unitguideSearchQuery,
+				elementText: 'Search Unit Guides'
+			};
+		}
+	};
+
+	// Add a button
+	function addButtonToElement({
+		parentElementId = 'ExternalLink',
+		elementClass = 'btn btn-link btn-sm btn-block quick-link-button',
+		elementHref,
+		elementText
+	}) {
+		document.getElementById(parentElementId).innerHTML +=
+			'<a class="' +
+			elementClass +
+			'" href="' +
+			elementHref +
+			'" role="button" target="_blank">' +
+			elementText +
+			'</a>';
+	}
+
+	function consoleHealthCheck(object, text) {
+		!Object.values(object).every((o) => o === null)
+			? console.log('%c PASSED ', 'color: white; background-color: #95B46A', text, object)
+			: console.log('%c ERROR ', 'color: white; background-color: #D33F49', text, object);
+	}
+
 	// ***************************************************** Variables ***************************************************** //
 
 	// Scraped & Dependent variables
@@ -110,6 +110,8 @@ var FITMOODLE = (function() {
 			? document.querySelector("a[data-key='grades']").getAttribute('href')
 			: null
 	};
+	consoleHealthCheck(unit, '@MS: Unit =');
+
 	const user = {
 		email: document.querySelector('.myprofileitem.email')
 			? document.querySelector('.myprofileitem.email').innerText.toLowerCase()
@@ -119,10 +121,12 @@ var FITMOODLE = (function() {
 			: null,
 		hasEditingAccess: document.querySelector("a[href*='&edit=']") ? true : false,
 		turnedEditingOn: document.querySelector('body.editing') ? true : false,
-		hasPowerUserAccess: function () {
-			return powerUsers.includes(user.email) || document.URL.indexOf(avoidRestrictionQuery) > 0 ? true : false;
-		}(),
+		hasPowerUserAccess: (function() {
+			return powerUsers.includes(this.email) || document.URL.indexOf(avoidRestrictionQuery) > 0 ? true : false;
+		})()
 	};
+	consoleHealthCheck(user, '@MS: User =');
+
 	const callista = {
 		nodelist: document.querySelectorAll('section.block_callista div.card-text a[onclick]'),
 		noCallista: document.querySelector('section.block_callista p'),
@@ -130,6 +134,8 @@ var FITMOODLE = (function() {
 			return this.nodelist.length > 0 ? [ ...this.nodelist ].map((x) => x.innerText) : this.noCallista.innerText;
 		}
 	};
+	consoleHealthCheck(callista, '@MS: Callista =');
+
 	var offering = new function() {
 		this.shortnameBlocks = unit.shortname.split('_');
 		this.unitCodes = this.shortnameBlocks[0].split('-'); // Handling multiple unit codes and teaching periods (e.g., FITXXXX-FITYYYY, S1-S2)
@@ -140,18 +146,19 @@ var FITMOODLE = (function() {
 		this.teachingFaculty = this.unitCodes[0].indexOf('MAT') > 0 ? 'Science' : 'FIT';
 		this.location = this.teachingPeriods[0].indexOf('MO-TP') > 0 ? 'Monash Online' : 'Campus';
 	}();
+	consoleHealthCheck(offering, '@MS: Offering =');
 
 	// ************************************************ Public Methods ***************************************************** //
 	// methods are avaiable for use
 	return {
-		logInfo: function() {
-			consoleHealthCheck(unit, '@MS: Unit =');
-			consoleHealthCheck(user, '@MS: User =');
-			consoleHealthCheck(offering, '@MS: Offering =');
-			consoleHealthCheck(callista, '@MS: Callista =');
-		},
 		setMoodlePowerUsers: function(emialArray) {
 			if (Array.isArray(emialArray)) powerUsers = emialArray;
+			consoleHealthCheck(powerUsers, '@MS: Power Users =');
+			return this;
+		},
+		setBypassRestriction: function (queryString) {
+			if (typeof queryString === 'string' || queryString instanceof String) this.bypassRestrictionQuery = queryString;
+			return this;
 		},
 		addStudentPortal: function() {
 			// add IT Student Portal
@@ -160,6 +167,7 @@ var FITMOODLE = (function() {
 					offering.location === 'Campus' ? moodlePortalButton('38028') : moodlePortalButton('24532')
 				);
 			}
+			return this;
 		},
 		addMyGrades: function() {
 			// add My Grades
@@ -169,6 +177,7 @@ var FITMOODLE = (function() {
 					elementText: 'My Grades'
 				});
 			}
+			return this;
 		},
 		addUnitGuide: function() {
 			if (offering.unitCodes[0].match(/\w{3}\d{4}/g) && callista.nodelist.length > 1) {
@@ -205,6 +214,130 @@ var FITMOODLE = (function() {
 					}
 				}
 			}
-		}
+			return this;
+		},
+		addTurnEditingButton: function() {
+			// Target the turn editing on/off menu item
+			var menu = $('.context-header-settings-menu a[href*="edit=o"]');
+
+			if (menu.length) {
+				console.log('@GB: menu = ', menu);
+				var html = $(menu).html();
+
+				// set different button themes for editing on or off
+				if (html.indexOf('Turn editing on') >= 0) {
+					var state = 'btn-primary';
+				} else {
+					var state = 'btn-success';
+				}
+				console.log('@GB: html = ', html);
+				var href = $(menu).attr('href');
+				console.log('@GB: href = ', href);
+				var btn =
+					'<a class="btn ' + state + '" id="edit-on-off" href="' + href + '"><span>' + html + '</span></a>';
+				console.log('@GB: btn = ', btn);
+
+				// Add button before breadcrumbs
+				$('#page-navbar').before(btn);
+			}
+			return this;
+		},
+		addSupportBee: function() {
+			// Add 'Contact support', but only if editing is on
+			if ($('body.editing').length > 0) {
+				// Load external script
+				$.getScript('//d3932137p5ikt7.cloudfront.net/widget_v3/loader.min.js')
+					.done(function(script, textStatus) {
+						sb_contact_form('initialize', {
+							company: 'fit-monash',
+							height: '500px',
+							position: 'bottom',
+							email: user.email,
+							name: user.fullName,
+							subject: "Moodle issue in '" + unit.shortname + ' (URL: ' + window.location + ')',
+							locale: 'en',
+							captcha: 'false',
+							forwarding_address_id: '26544'
+						});
+					})
+					.fail(function(jqxhr, settings, exception) {
+						console.log('@GB: exception = ', exception);
+						console.log('@GB: settings = ', settings);
+					});
+			}
+			return this;
+		},
+		addBackToTop: function() {
+			if ($('#back-to-top').length) {
+				var scrollTrigger = 100, // px
+					backToTop = function() {
+						var scrollTop = $(window).scrollTop();
+						if (scrollTop > scrollTrigger) {
+							$('#back-to-top').addClass('show');
+						} else {
+							$('#back-to-top').removeClass('show');
+						}
+					};
+				backToTop();
+				$(window).on('scroll', function() {
+					backToTop();
+				});
+				$('#back-to-top').on('click', function(e) {
+					e.preventDefault();
+					$('html,body').animate(
+						{
+							scrollTop: 0
+						},
+						700
+					);
+				});
+			}
+			return this;
+		},
+		limitUnitMainPage: function() {
+			if (window.location.href.indexOf('view.php?id=') > 0 && $('body.editing').length > 0) {
+				$("a[title='Set or change image']").css('display', 'none');
+				$("a[title='Edit summary']").css('display', 'none');
+				$("a[title='Move this section out of the grid']").css('display', 'none');
+				$('a.add-sections').css('display', 'none');
+				$("i[aria-label='Hide HTML block']").parent().hide();
+				$("i[aria-label='Hide Panopto block']").parent().hide();
+				$("i[aria-label='Hide Logged in user block']").parent().hide();
+			}
+			return this;
+		},
+		limitOtherUsers: function() {
+			if (window.location.href.indexOf('enrol/otherusers.php?id=') > 0) {
+				$("button[type='submit']").css('display', 'none');
+				$('a.assignrolelink').css('display', 'none');
+			}
+			return this;
+		},
+		changeDefaultImport: function() {
+			if (window.location.href.indexOf('backup/import.php') > 0) {
+				$("input[type='checkbox']").prop('checked', false);
+				$('input#id_setting_root_activities').prop('checked', true);
+			}
+			return this;
+		},
+		changeNewActivityIcon: function() {
+			$('img.new_activity').attr(
+				'src',
+				'https://lms.monash.edu/draftfile.php/3075058/user/draft/44115094/New-Activity.png'
+			);
+			return this;
+		},
+		hideBlockAll: function() {
+			if (
+				(window.location.href.indexOf('action=grading') > 0 &&
+					window.location.href.indexOf('mod/assign/view.php') > 0) ||
+				window.location.href.indexOf('/grade/report/history/index.php') > 0 ||
+				window.location.href.indexOf('grade/report/grader/index.php') > 0
+			) {
+				$("section[data-region='blocks-column']").hide();
+				$('section#region-main').removeClass('has-blocks');
+			}
+			return this;
+		},
 	};
 })();
