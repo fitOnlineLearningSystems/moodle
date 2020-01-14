e987c0c5f5ecacab645f03140715b62843b21822
Milad Sayad <miladsayad@gmail.com>
2020-01-14 13:11:48 +1100

    fixed shortname with space

--- dist/moodle.js
+++ dist/moodle.js
@@ -67,7 +67,7 @@ var FITMOODLE = (function() {
 			: null;
 		if (!User.email) {
 			User.email = document.querySelector('.myprofileitem.city')
-				? document.querySelector('.myprofileitem.city').innerText.toLowerCase().replace("email address: ", "")
+				? document.querySelector('.myprofileitem.city').innerText.toLowerCase().replace('email address: ', '')
 				: null;
 		}
 		User.fullName = document.querySelector('.myprofileitem.fullname')
@@ -97,15 +97,17 @@ var FITMOODLE = (function() {
 		 * @class Offering
 		 */
 	function getOffering() {
-		Offering.shortnameBlocks = Unit.shortname.split('_');
-		Offering.unitCodes = Offering.shortnameBlocks[0].split('-'); // Handling multiple unit codes and teaching periods (e.g., FITXXXX-FITYYYY, S1-S2)
-		Offering.teachingPeriodBlock = Offering.shortnameBlocks[1];
-		Offering.teachingPeriods = Offering.teachingPeriodBlock.split('-');
-		Offering.campus =
-			Offering.shortnameBlocks.length > 3 ? Offering.shortnameBlocks[2].split('-') : 'One for all campuses';
-		Offering.year = Offering.shortnameBlocks[Offering.shortnameBlocks.length - 1].split('-');
-		Offering.taughtByFIT = Offering.unitCodes[0].indexOf('FIT') >= 0 ? true : false;
-		Offering.monashOnline = Offering.teachingPeriods[0].indexOf('MO-TP') > 0 ? true : false;
+		if (/_/g.test(Unit.shortname).length > 1) {
+			Offering.shortnameBlocks = Unit.shortname.split('_');
+			Offering.unitCodes = Offering.shortnameBlocks[0].split('-'); // Handling multiple unit codes and teaching periods (e.g., FITXXXX-FITYYYY, S1-S2)
+			Offering.teachingPeriodBlock = Offering.shortnameBlocks[1];
+			Offering.teachingPeriods = Offering.teachingPeriodBlock.split('-');
+			Offering.campus =
+				Offering.shortnameBlocks.length > 3 ? Offering.shortnameBlocks[2].split('-') : 'One for all campuses';
+			Offering.year = Offering.shortnameBlocks[Offering.shortnameBlocks.length - 1].split('-');
+			Offering.taughtByFIT = Offering.unitCodes[0].indexOf('FIT') >= 0 ? true : false;
+			Offering.monashOnline = Offering.teachingPeriods[0].indexOf('MO-TP') > 0 ? true : false;
+		}
 	}
 
 	/**
